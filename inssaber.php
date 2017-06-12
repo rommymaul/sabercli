715e0ed954b9c2b7b13a3b3d80cca467219df90b
Rommy Maulana <t1kus90tofficial@gmail.com>
2017-06-12 13:15:52 +0700

    Create inssaber.php

--- inssaber.php
+++ inssaber.php
@@ -1,12 +1,19 @@
 <?php
 //installing packgages
-system("apt install wget curl jhead vim unzip -y \n");
+echo "installing packgages\n";
+sleep(1);
+system("apt install wget curl jhead vim unzip -y");
+echo "\n";
 
 //new path of saber v.1
 $new = "/data/data/com.termux/files/usr/bin/";
 
 //copying saber v.1 to new path
+echo "\n copying saber\n";
+sleep(1);
 system("cp saber $new");
 
 //checking if saber v.1 already copied
+echo "\n checking saber\n";
+sleep(1);
 echo (is_file('/data/data/com.termux/files/usr/bin/saber')) ? "success to install,\n run the tools with command saber\n" : "failed to install\n";
